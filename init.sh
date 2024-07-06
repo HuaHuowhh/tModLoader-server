4064858d9d1a920b2a1c6b5df2d77f1598b28f88
Fireworks <1773931858@qq.com>
2024-07-06 19:40:12 +0800

    Add files via upload

--- init.sh
+++ init.sh
@@ -4,8 +4,8 @@ pkg install unzip -y
 pkg install mono -y
 
 curl -O https://file.upfile.live/uploads/20240706/8432e0e748a2011b6f5f585c9e6f8aeb.zip
-unzip terraria-server-1449.zip
-rm terraria-server-1449.zip
+unzip 1449.zip
+rm 1449.zip
 chmod +x 1449/Linux/TerrariaS*
 rm 1449/Linux/System*
 rm 1449/Linux/Mono*
