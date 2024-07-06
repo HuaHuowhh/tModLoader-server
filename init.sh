b4f9d380f5caa82269890e78b538cd9328802c09
Fireworks <1773931858@qq.com>
2024-07-06 16:22:57 +0800

    Add files via upload

---
+++ init.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+apt upgrade -y && apt update
+pkg install unzip -y
+pkg install mono -y
+
+curl -O https://github.com/tModLoader/tModLoader/releases/download/v0.11.8.9/tModLoader.Linux.v0.11.8.9.zip
+unzip tModLoader.Linux.v0.11.8.9.zip
+rm tModLoader.Linux.v0.11.8.9.zip
+chmod +x tModLoaderS*
+rm System*
+rm Mono*
+rm monoconfig
+rm mscorlib.dll
+
+
+touch ~/../usr/bin/Terra
+chmod +x ~/../usr/bin/Terra
+echo -e "#!/bin/bash\nmono ${PWD}/tModLoaderServer.exe" > ~/../usr/bin/Terra
+Terra
