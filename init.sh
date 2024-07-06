265396988ebc62ff2945ed53af6c4c87f7ea413b
Fireworks <1773931858@qq.com>
2024-07-06 19:30:06 +0800

    Add files via upload

--- init.sh
+++ init.sh
@@ -3,13 +3,18 @@ apt upgrade -y && apt update
 pkg install unzip -y
 pkg install mono -y
 
-curl -O https://github.com/tModLoader/tModLoader/releases/download/v0.11.8.9/tModLoader.Linux.v0.11.8.9.zip
-unzip tModLoader.Linux.v0.11.8.9.zip
-rm tModLoader.Linux.v0.11.8.9.zip
-chmod +x tModLoaderS*
-
+curl -O https://terraria.org/api/download/pc-dedicated-server/terraria-server-1449.zip
+unzip terraria-server-1449.zip
+rm terraria-server-1449.zip
+chmod +x 1449/Linux/TerrariaS*
+rm 1449/Linux/System*
+rm 1449/Linux/Mono*
+rm 1449/Linux/monoconfig
+rm 1449/Linux/mscorlib.dll
+rm -rf 1449/Mac
+rm -rf 1449/Windows
 
 touch ~/../usr/bin/Terra
 chmod +x ~/../usr/bin/Terra
-echo -e "#!/bin/bash\nmono ${PWD}/tModLoaderServer.exe" > ~/../usr/bin/Terra
+echo -e "#!/bin/bash\nmono ${PWD}/1449/Linux/tModLoaderServer.exe" > ~/../usr/bin/Terra
 Terra
