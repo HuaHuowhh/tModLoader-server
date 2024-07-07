fa28a0e0ed24c56db168cce2b164717c2dba212b
Fireworks <1773931858@qq.com>
2024-07-07 14:16:27 +0800

    Add files via upload

--- init.sh
+++ init.sh
@@ -3,11 +3,14 @@ apt upgrade -y && apt update
 pkg install unzip -y
 pkg install mono -y
 
-curl -O https://file.upfile.live/uploads/20240706/d3c452473399161bee1338c19402048e.zip
+curl -O https://file.upfile.live/uploads/20240707/d3c452473399161bee1338c19402048e.zip
 unzip d3c452473399161bee1338c19402048e.zip
 rm d3c452473399161bee1338c19402048e.zip
 chmod +x 1449/Linux/TerrariaS*
-
+rm 1449/Linux/System*
+rm 1449/Linux/Mono*
+rm 1449/Linux/monoconfig
+rm 1449/Linux/mscorlib.dll
 rm -rf 1449/Mac
 rm -rf 1449/Windows
 
