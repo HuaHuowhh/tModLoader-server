c4c308b9318b5c187e18396e715fad779a966ab5
Fireworks <1773931858@qq.com>
2024-07-09 13:07:04 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -4,7 +4,7 @@ pkg install unzip -y
 pkg install mono -y
 
 curl -O https://file.upfile.live/uploads/20240709/ad800d84a8ba14e8c13efb3eed714384.zip
-unzip ad800d84a8ba14e8c13efb3eed714384
+unzip ad800d84a8ba14e8c13efb3eed714384.zip
 rm ad800d84a8ba14e8c13efb3eed714384.zip
 chmod +x 1449/Linux/TerrariaS*
 rm 1449/Linux/System*
